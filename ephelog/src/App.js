f105487c7130bc15753aaead4ded6f38417125eb
Nooha <noohakottangodan@gmail.com>
2022-10-27 11:56:28 +0530

    added tailwind css

--- ephelog/src/App.js
+++ ephelog/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
